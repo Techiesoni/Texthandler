5ff5b6aa4420498b3cfb3fa6d3ebd6e08a202ce8
Techiesoni <shubhamsoni291@gmail.com>
2022-07-20 16:45:35 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Navbar from './components/Navbar';
+ 
+function App() {
+  return (
+    <>
+    <Navbar title="TextUtils" aboutText="About"/>
+    {/* <Navbar/> */}
+    </>
+  
+  );
+}
+
+export default App;
